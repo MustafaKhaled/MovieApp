43965f897e8f3e483b87000c2ced6278f0c939ba
mustafa.khaled <mustafa.khaled@intercom.com.eg>
2019-10-16 09:13:47 +0200

    edited details pojo

--- app/src/main/java/com/movieapp/kotlin/domain/model/details/BelongsToCollection.kt
+++ app/src/main/java/com/movieapp/kotlin/domain/model/details/BelongsToCollection.kt
@@ -1,5 +1,4 @@
 package com.movieapp.kotlin.domain.model.details
-
 import com.google.gson.annotations.SerializedName
 
 /*
@@ -14,31 +13,10 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar */
 
 
-data class MovieDetails (
+data class BelongsToCollection (
 
-	@SerializedName("adult") val adult : Boolean,
-	@SerializedName("backdrop_path") val backdrop_path : String,
-	@SerializedName("belongs_to_collection") val belongs_to_collection : BelongsToCollection,
-	@SerializedName("budget") val budget : Int,
-	@SerializedName("genres") val genres : List<Genres>,
-	@SerializedName("homepage") val homepage : String,
 	@SerializedName("id") val id : Int,
-	@SerializedName("imdb_id") val imdb_id : String,
-	@SerializedName("original_language") val original_language : String,
-	@SerializedName("original_title") val original_title : String,
-	@SerializedName("overview") val overview : String,
-	@SerializedName("popularity") val popularity : Double,
+	@SerializedName("name") val name : String,
 	@SerializedName("poster_path") val poster_path : String,
-	@SerializedName("production_companies") val production_companies : List<ProductionCompanies>,
-	@SerializedName("production_countries") val production_countries : List<ProductionCountries>,
-	@SerializedName("release_date") val release_date : String,
-	@SerializedName("revenue") val revenue : Int,
-	@SerializedName("runtime") val runtime : Int,
-	@SerializedName("spoken_languages") val spoken_languages : List<SpokenLanguages>,
-	@SerializedName("status") val status : String,
-	@SerializedName("tagline") val tagline : String,
-	@SerializedName("title") val title : String,
-	@SerializedName("video") val video : Boolean,
-	@SerializedName("vote_average") val vote_average : Double,
-	@SerializedName("vote_count") val vote_count : Int
+	@SerializedName("backdrop_path") val backdrop_path : String
 )
